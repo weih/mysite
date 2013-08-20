d5e4d146d030258d60a082c838878261f3a782e2
Hao Wei <imwillmouse@gmail.com>
2013-08-20 10:10:53 +0800

    test some sinatra basic functionality

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require 'sinatra/base'
 module SinatraBone
   class App < Sinatra::Base
     get '/' do
+      @title = "Willmouse's Homepage"
       haml :index
+    end
+    get '/post/:post_name' do |post_name|
+      "you are at #{post_name}"
+      # haml :psot
+    end
+    get '/comment/*/*' do
+      @comments = params[:splat].join(' ')
+      "Comment -> #{@comments}"
     end
   end
 end
